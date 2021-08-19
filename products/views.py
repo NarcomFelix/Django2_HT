56c39d93898f22b22f18e835b7c51ada55bd5d4f
PavelZh <zidkihpv@gmail.com>
2021-08-19 16:54:22 +0300

    added products.json

--- products/views.py
+++ products/views.py
@@ -1,9 +1,20 @@
+import os
+import json
+
 from django.shortcuts import render
+
+MODULE_DIR = os.path.dirname(__file__)
+
 
 def index(request):
     context = {'title': "GeekShop"}
     return render(request, 'products/index.html', context)
 
 def products(request):
-    context = {'title': 'GeekShop - Каталог'}
+    file_path = os.path.join(MODULE_DIR, 'fixtures/products.json')
+    context = {
+        'title': 'GeekShop - Каталог',
+        'products': json.load(open(file_path, encoding='utf-8')),
+    }
+
     return render(request, 'products/products.html', context)
