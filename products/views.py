b7cb39853d580cf91f44b9f89797f83804d27368
PavelZh <zidkihpv@gmail.com>
2021-08-21 11:05:40 +0300

    changed name goods.json and changed views.py

--- products/views.py
+++ products/views.py
@@ -11,7 +11,7 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 def products(request):
-    file_path = os.path.join(MODULE_DIR, 'fixtures/products.json')
+    file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')
     context = {
         'title': 'geekShop - Каталог',
         'products': json.load(open(file_path, encoding='utf-8')),
