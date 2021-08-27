7b2e97332c17adc70d78a70d8414005500245e93
PavelZh <zidkihpv@gmail.com>
2021-08-27 12:16:28 +0300

    the same as last, but work

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from users.views import login, registration
 
-app_name = 'products'
+app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('registration/', registration, name='registration')
+    path('registration/', registration, name='registration'),
 ]
