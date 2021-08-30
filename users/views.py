90fc4105c98ca37d041f9c191da36c779412289e
PavelZh <zidkihpv@gmail.com>
2021-08-31 00:14:37 +0300

    added POST request for profile controller

--- users/views.py
+++ users/views.py
@@ -33,7 +33,13 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 def profile(request):
-    form = UserProfileForm(instance=request.user)
+    if request.method == 'POST':
+        form =UserProfileForm(instance=request.user, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
     context = {'title': 'GeekShop - Личный кабинет', 'form': form}
     return render(request, 'users/profile.html', context)
 
