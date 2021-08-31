b50164f07a3a6e50c2f40444353acd5562f1dd59
PavelZh <zidkihpv@gmail.com>
2021-08-31 19:00:15 +0300

    fix bug

--- users/views.py
+++ users/views.py
@@ -34,16 +34,17 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 def profile(request):
+    user = request.user
     if request.method == 'POST':
-        form =UserProfileForm(instance=request.user, files=request.FILES, data=request.POST)
+        form =UserProfileForm(instance=user, files=request.FILES, data=request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('users:profile'))
     else:
-        form = UserProfileForm(instance=request.user)
+        form = UserProfileForm(instance=user)
     context = {'title': 'GeekShop - Личный кабинет',
                'form': form,
-               'baskets': Basket.objects.all()
+               'baskets': Basket.objects.filter(user=user),
                }
     return render(request, 'users/profile.html', context)
 
